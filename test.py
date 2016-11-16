30b9b56e5c40e55c59b275027b58cb64e9275be8
firber <wangjingze1987@gmail.com>
2016-11-16 16:41:05 +0800

    3.1 Traversing a Single Domain

--- test.py
+++ test.py
@@ -1,9 +1,16 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import re
+import datetime
+import random
 
-html = urlopen("http://baike.baidu.com/view/1256.htm")
-bs0bj = BeautifulSoup(html,"html.parser")
-for link in bs0bj.find("body").findAll("a", href=re.compile("^(\/subview\/|\/view\/)[0-9\/]*")):
-    if 'href' in link.attrs:
-        print("http://baike.baidu.com" + link.attrs['href'])
+random.seed(datetime.datetime.now())
+def getLinks(articleUrl):
+    html = urlopen("http://baike.baidu.com" + articleUrl)
+    bs0bj = BeautifulSoup(html,"html.parser")
+    return bs0bj.find("body").findAll("a", href=re.compile("^(\/subview\/|\/view\/)[0-9\/]*"))
+links = getLinks("/view/1256")
+while len(links) > 0:
+    newArticle = links[random.randint(0, len(links)-1)].attrs["href"]
+    print(newArticle)
+    links = getLinks(newArticle)
