9c64b3eca3a5abfde8de1a66efeeb0d01c87559b
firber <wangjingze1987@gmail.com>
2016-11-16 16:57:06 +0800

    crawling an Entire site

--- test.py
+++ test.py
@@ -4,13 +4,16 @@ import re
 import datetime
 import random
 
-random.seed(datetime.datetime.now())
-def getLinks(articleUrl):
-    html = urlopen("http://baike.baidu.com" + articleUrl)
-    bs0bj = BeautifulSoup(html,"html.parser")
-    return bs0bj.find("body").findAll("a", href=re.compile("^(\/subview\/|\/view\/)[0-9\/]*"))
-links = getLinks("/view/1256")
-while len(links) > 0:
-    newArticle = links[random.randint(0, len(links)-1)].attrs["href"]
-    print(newArticle)
-    links = getLinks(newArticle)
+pages = set()
+def getLinks(pageUrl):
+    global pages
+    html = urlopen("http://baike.baidu.com" + pageUrl)
+    bs0bj = BeautifulSoup(html)
+    for link in bs0bj.find("body").findAll("a", href=re.compile("^(\/subview\/|\/view\/)[0-9\/]*")):
+        if 'href' in link.attrs:
+            if link.attrs['href'] not in pages:
+                newpage = link.attrs['href']
+                print(newpage)
+                pages.add(newpage)
+                getLinks(newpage)
+getLinks("")
