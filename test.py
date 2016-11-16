5e51d98f3306178a06f9066d03c44f4e4b20fda9
firber <wangjingze1987@gmail.com>
2016-11-16 23:28:33 +0800

    'da'

--- test.py
+++ test.py
@@ -8,12 +8,17 @@ pages = set()
 def getLinks(pageUrl):
     global pages
     html = urlopen("http://baike.baidu.com" + pageUrl)
-    bs0bj = BeautifulSoup(html)
+    bs0bj = BeautifulSoup(html, "html.parser")
+    try:
+        print(bs0bj.h1.get_text())
+    except AttributeError:
+        print("未在该页面找到主题词！")
+
     for link in bs0bj.find("body").findAll("a", href=re.compile("^(\/subview\/|\/view\/)[0-9\/]*")):
         if 'href' in link.attrs:
             if link.attrs['href'] not in pages:
                 newpage = link.attrs['href']
-                print(newpage)
+                print("------------\n"+newpage)
                 pages.add(newpage)
                 getLinks(newpage)
 getLinks("")
